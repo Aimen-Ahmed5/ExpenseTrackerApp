da1b4bcb7e0b337d178ce41e12aef0a1b5a485a7
Aimen-Ahmed5 <aimenahmed63@gmail.com>
2022-02-13 16:35:59 +0500

    component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  // using date object 
+  let expenseDate = new Date(2022, 1, 13);
+  let expenseTitle = "School Fee";
+  let expenseAmount = 300;
+
   return (
-    <div className="">
-      
+    <div>
+      <h2>Expense Tracker</h2>
+       {/* props: passing data to components */}
+    <ExpenseItem date={expenseDate} title={expenseTitle} amount={expenseAmount} ></ExpenseItem>
+
     </div>
   );
 }
