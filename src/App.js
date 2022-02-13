c7f0f3789cb47450f7a4c60b32b3c9ad5059c6a0
Aimen-Ahmed5 <aimenahmed63@gmail.com>
2022-02-13 17:01:15 +0500

    data passed to components using array

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   // let expenseTitle = "School Fee";
   // let expenseAmount = 300;
 
+  // array expense with 4 objects having data(properties) for 4 components 
   let expenses = [
     {
       id: 'i1',
@@ -37,10 +38,12 @@ function App() {
     <div>
       <h2>Expense Tracker</h2>
        {/* props: passing data to components */}
-    <ExpenseItem date={expenseDate} title={expenseTitle} amount={expenseAmount} ></ExpenseItem>
-    <ExpenseItem date={expenseDate} title={expenseTitle} amount={expenseAmount} ></ExpenseItem>
-    <ExpenseItem date={expenseDate} title={expenseTitle} amount={expenseAmount} ></ExpenseItem>
-    <ExpenseItem date={expenseDate} title={expenseTitle} amount={expenseAmount} ></ExpenseItem>
+    {/* <ExpenseItem date={expenseDate} title={expenseTitle} amount={expenseAmount} ></ExpenseItem> */}
+
+    <ExpenseItem date={expenses[0].date} title={expenses[0].title} amount={expenses[0].amount} ></ExpenseItem>
+    <ExpenseItem date={expenses[1].date} title={expenses[1].title} amount={expenses[1].amount} ></ExpenseItem>
+    <ExpenseItem date={expenses[2].date} title={expenses[2].title} amount={expenses[2].amount} ></ExpenseItem>
+    <ExpenseItem date={expenses[3].date} title={expenses[3].title} amount={expenses[3].amount} ></ExpenseItem>
     </div>
   );
 }
