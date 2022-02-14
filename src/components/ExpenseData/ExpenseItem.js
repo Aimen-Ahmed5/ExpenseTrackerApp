5bc994d8ac67f3b49bf95aaec379368f33480e60
Aimen-Ahmed5 <aimenahmed63@gmail.com>
2022-02-14 21:28:01 +0500

    useState hook used

--- src/components/ExpenseData/ExpenseItem.js
+++ src/components/ExpenseData/ExpenseItem.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { useState} from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
 
+    // useState will return array that has 1. variable 2. function (name is defined accordingly)
+    // array destructuing is assigning variable to to array value 
+    const [title, setTitle] = useState(props.title);
+
     const clickHandler = () => {
-        alert("Clicked");
+       setTitle('New title..');
     }
 
     return(
         <Card className="expense-item">
             <ExpenseDate date = {props.date}/>
             <div className="expense-item_description">
-                <h2>{props.title}</h2>
+                <h2>{ title }</h2>
                 <div className="expense-item_price">${props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change title</button>
