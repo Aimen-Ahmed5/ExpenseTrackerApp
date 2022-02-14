f946274691d72dc01006469a9d4cad1612308bf6
Aimen-Ahmed5 <aimenahmed63@gmail.com>
2022-02-14 20:50:21 +0500

    alet message display when button clicked

--- src/components/ExpenseData/ExpenseItem.js
+++ src/components/ExpenseData/ExpenseItem.js
@@ -4,7 +4,11 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
-    
+
+    const clickHandler = () => {
+        alert("Clicked");
+    }
+
     return(
         <Card className="expense-item">
             <ExpenseDate date = {props.date}/>
@@ -12,6 +16,7 @@ const ExpenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className="expense-item_price">${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change title</button>
         </Card>
     );
 }
