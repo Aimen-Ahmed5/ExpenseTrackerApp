244a7beb3cf83c159b2d6d185bd78f484ad5b9fe
Aimen-Ahmed5 <aimenahmed63@gmail.com>
2022-02-14 15:40:00 +0500

    expense data on UI with necesary import statements

---
+++ src/components/UI/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Card.css';
+
+function Card(props){
+    // props.className will have class which is in the component
+    const classes = 'card' + props.className;
+
+    // data in Card component will be copied in .children
+    return <div className={classes}>{props.children}</div>   
+
+}
+
+export default Card;
