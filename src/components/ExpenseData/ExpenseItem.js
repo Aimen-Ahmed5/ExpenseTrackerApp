93d60fb68e7f97c91fd04f33482f2d97f294f7d4
Aimen-Ahmed5 <aimenahmed63@gmail.com>
2022-02-15 23:27:37 +0500

    some code commented as separate form is designed

--- src/components/ExpenseData/ExpenseItem.js
+++ src/components/ExpenseData/ExpenseItem.js
@@ -8,27 +8,28 @@ const ExpenseItem = (props) => {
     // useState will return array that has 1. variable 2. function (name is defined accordingly)
     // array destructuing is assigning variable to to array value 
 
-    const [new_title, setNew_title] = useState("");
+    // const [new_title, setNew_title] = useState("");
 
-    const [title, setTitle] = useState(props.title);
+    // const [title, setTitle] = useState(props.title);
 
-    const clickHandler = () => {
-       setTitle(new_title);
-    }
+    // const clickHandler = () => {
+    //    setTitle(new_title);
+    // }
 
-    const changeHandler = (event) => {
-        setNew_title(event.target.value);
-    }
+    // const changeHandler = (event) => {
+    //     setNew_title(event.target.value);
+    // }
 
     return(
         <Card className="expense-item">
             <ExpenseDate date = {props.date}/>
             <div className="expense-item_description">
-                <h2>{ title }</h2>
+                {/* <h2>{ title }</h2> */}
+                <h2>{props.title}</h2>
                 <div className="expense-item_price">${props.amount}</div>
             </div>
-            <input type="text" value={new_title} onChange={changeHandler}/>
-            <button onClick={clickHandler}>Change title</button>
+            {/* <input type="text" value={new_title} onChange={changeHandler}/>
+            <button onClick={clickHandler}>Change title</button> */}
         </Card>
     );
 }
