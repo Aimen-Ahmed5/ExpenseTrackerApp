7b794646cd61451a420730d5bdb1a8fbf66e1bce
Aimen-Ahmed5 <aimenahmed63@gmail.com>
2022-02-15 22:32:13 +0500

    button and event used to update expense title on UI

--- src/components/ExpenseData/ExpenseItem.js
+++ src/components/ExpenseData/ExpenseItem.js
@@ -7,10 +7,17 @@ const ExpenseItem = (props) => {
 
     // useState will return array that has 1. variable 2. function (name is defined accordingly)
     // array destructuing is assigning variable to to array value 
+
+    const [new_title, setNew_title] = useState("");
+
     const [title, setTitle] = useState(props.title);
 
     const clickHandler = () => {
-       setTitle('New title..');
+       setTitle(new_title);
+    }
+
+    const changeHandler = (event) => {
+        setNew_title(event.target.value);
     }
 
     return(
@@ -20,6 +27,7 @@ const ExpenseItem = (props) => {
                 <h2>{ title }</h2>
                 <div className="expense-item_price">${props.amount}</div>
             </div>
+            <input type="text" value={new_title} onChange={changeHandler}/>
             <button onClick={clickHandler}>Change title</button>
         </Card>
     );
