18e948afcd4810784a8869914519fa2dd995b895
Aimen-Ahmed5 <aimenahmed63@gmail.com>
2022-02-16 18:54:30 +0500

    multiple states and events used

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -1,23 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+
+    const [enteredTitle, setEnteredTitle] = useState("");
+    const [enteredAmount, setEnteredAmount] = useState("");
+    const [enteredDate, setEnteredDate] = useState("");
+
+    const titleChangeHandler = (event) => {
+        // console.log(event.target.value);
+        setEnteredTitle(event.target.value);
+    }
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+    }
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+    }
+
     return(
             <form>
                 <div className="new-expense_controls">
                     <div className="control">
                         <label>Title</label>
-                        <input type="text" />
+                        <input type="text" onChange={titleChangeHandler} />
                     </div>
 
                     <div className="control">
                         <label>Amount</label>
-                        <input type="number" min="0.01" step="0.01"/>
+                        <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
                     </div>
 
                     <div className="control">
                         <label>Date</label>
-                        <input type="date" />
+                        <input type="date" onChange={dateChangeHandler}/>
                     </div>
                 </div>
                 <div className="new-expense_action" >
