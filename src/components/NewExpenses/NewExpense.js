dce676210706150ce5b272d6e6c9ebe6052a1e2f
Aimen-Ahmed5 <aimenahmed63@gmail.com>
2022-02-16 22:02:42 +0500

    send data to its parent thats is App.js through props

--- src/components/NewExpenses/NewExpense.js
+++ src/components/NewExpenses/NewExpense.js
@@ -2,10 +2,26 @@ import React from "react";
 import './NewExpense.css';
 import ExpenseForm from "./ExpenseForm";
 
-const NewExpense = () =>{
+// to access props from App.js
+const NewExpense = (props) =>{
+
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+
+        //object
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        }
+
+        // onAddExpense is in App.js
+        props.onAddExpense(expenseData);
+
+        console.log(expenseData);
+    }
+
     return(
         <div className="new-expense">
-            <ExpenseForm/>
+            <ExpenseForm onSaveExpenseData = {saveExpenseDataHandler}/>
         </div>
 
     );
