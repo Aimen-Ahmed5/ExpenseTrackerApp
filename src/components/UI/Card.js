18f046907347f4fef5e2de447d517fdf010604bd
Aimen-Ahmed5 <aimenahmed63@gmail.com>
2022-02-18 21:17:45 +0500

    bug removed, Output is perfectly fine

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -3,7 +3,7 @@ import './Card.css';
 
 const Card = (props) => {
     // props.className will have class which is in the component
-    const classes = 'card' + props.className;
+    const classes = "card " + props.className;
 
     // data in Card component will be copied in .children
     return <div className={classes}>{props.children}</div>   
